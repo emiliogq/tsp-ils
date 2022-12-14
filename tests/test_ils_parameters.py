bd2a075c01614fb0f6211c71dc9444386d7e6c6a
Emilio Garcia <github@emiliogq.com>
2022-12-14 21:40:01 +0100

    Use Berlin52 file instead

--- tests/test_ils_parameters.py
+++ tests/test_ils_parameters.py
@@ -1,10 +1,14 @@
 from common import *
 from pytest import approx
 
+
+best_known_solution = 7542
+data = read_nodes_file("data/berlin52.tsp", 6, 58)
+
 def test_ils_parameters():
     
-    min_result = find_best_parameters(berlin52)
+    min_result = find_best_parameters(data)
     
     min_cost = min_result[2]
     print(f"The result with minimum cost found is {min_result}")
-    assert min_cost == approx(berlin52_best_known_solution_cost, rel=0.02)
+    assert min_cost == approx(best_known_solution, rel=0.02)
